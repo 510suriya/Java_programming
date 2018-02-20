8993bb5e2fd0ccb2f8c8129ffc060717cf209a3e
510suriya <36623890+510suriya@users.noreply.github.com>
2018-02-20 11:39:29 +0530

    Update num.py

--- num.py
+++ num.py
@@ -1,19 +1,13 @@
-	n = input("enter n value")
-	if(n<=100000):
-	{
-	if(n==0):
-	print ("zero")
-	elif(n<0):
-	print ("negative")
-	else:
-	print ("positive")
-	}
-	elif(n>=100000):
-    {
-    	print ("No exceed the limit")
-    }
-    else:
-    {
-    	printf("invalid");
-    }
-	
+ n=input("enter")
+  if (n<=100000):
+   if(n==0):
+    print "zero"
+   elif (n>0):
+        print "positive"
+   else:
+            print "negative"
+
+  elif (n>100000):
+    print "number exceed limit"
+  else:
+    print "Invalid"
