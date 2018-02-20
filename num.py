264b7e89706029fe582b3cad7fdb8e5ed348df9c
510suriya <36623890+510suriya@users.noreply.github.com>
2018-02-20 10:51:48 +0530

    Create new file

---
+++ num.py
@@ -0,0 +1,19 @@
+	n = input("enter n value")
+	if(n<=100000):
+	{
+	if(n==0):
+	print ("zero")
+	elif(n<0):
+	print ("negative")
+	else:
+	print ("positive")
+	}
+	elif(n>=100000):
+    {
+    	print ("No exceed the limit")
+    }
+    else:
+    {
+    	printf("invalid");
+    }
+	
