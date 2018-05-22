e326f6ad02dcb9011e570205a6f8e3b2457754dc
510suriya <36623890+510suriya@users.noreply.github.com>
2018-05-22 15:08:20 +0530

    Create eveinter.c

---
+++ eveinter.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+int main()
+{
+    int n1,n2, i;
+    scanf("%d",&n1);
+    scanf("%d",&n2);
+    if(n1<=10000&&n2<=10000){
+
+    for(i=n1; i<=n2; i++)
+    {
+    
+        if(i%2==0)
+        {
+            
+            printf("%d\n",i);
+            
+        }
+    }
+    }
+
+    
+    return 0;
+}
