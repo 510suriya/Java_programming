1ff63b961876bfb18ba0e6c88ca2f89a8181b6a4
510suriya <36623890+510suriya@users.noreply.github.com>
2018-05-28 20:15:37 +0530

    Update space.java

--- space.java
+++ space.java
@@ -7,11 +7,17 @@ public class WordCount
         int count=0;
         Scanner s = new Scanner(System.in);
         String str=s.nextLine();
+        if(str.length()<=1000){
         for(int i = 0; i < str.length(); i++)
         {
         if(Character.isWhitespace(str.charAt(i))) 
         count++;
         }
         System.out.println(+count);
+        }
+        else
+        {
+            System.out.println("invalid");
+        }
     } 
 }
