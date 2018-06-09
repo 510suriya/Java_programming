7492a2b61a279b240638f07e96b4fa96466d277a
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-09 20:55:02 +0530

    Create str_con.java

---
+++ str_con.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class Concat{
+
+     public static void main(String []args){
+         
+        Scanner s=new Scanner(System.in);
+        String str1=s.next();
+        String str2=s.next();
+        System.out.println(str1+ " " + str2);
+     }
+     
+}
