d4a365f7bfa5032ca2aad6baa275c9a05d0ae50e
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-09 21:10:21 +0530

    Create dig_count.java

---
+++ dig_count.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Range{
+
+     public static void main(String []args){
+         
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int n1 = String.valueOf(n).length();
+        System.out.println(n1);
+        
+     }
+     
+}
