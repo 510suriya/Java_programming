98eda33ca223f7959e0d6dde826b5a87adaf4ae1
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-09 21:13:22 +0530

    Create inc_one.java

---
+++ inc_one.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Incby{
+
+     public static void main(String []args){
+         
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int n1=n+1;
+        System.out.println(n1);
+        
+     }
+     
+}
