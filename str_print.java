b906a5acf8bf601d7db6045ccd0f669162bfcbd7
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-09 19:51:53 +0530

    Create str_print.java

---
+++ str_print.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class HelloWorld{
+
+     public static void main(String []args){
+         
+        Scanner s=new Scanner(System.in);
+        String str=s.next();
+        int n=s.nextInt();
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(str);
+        }
+     }
+}
