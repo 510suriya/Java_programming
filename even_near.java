0efcfd74436230498daf6805eb793f8036b164e9
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-11 12:39:42 +0530

    Create even_near.java

---
+++ even_near.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Eve_near{
+    public static void main(String args[]){
+    int n, sum = 0;
+    Scanner s=new Scanner(System.in);
+    n=s.nextInt();
+    if(n%2==0){
+        System.out.println(n);
+    }
+    else{
+        n=n-1;
+        System.out.println(n);
+        
+    }
+    }
+}
