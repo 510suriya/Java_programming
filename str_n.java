b7616425c86615a31ec66b92e99e5f19129e7dfe
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-23 10:43:38 +0530

    Update str_n.java

--- str_n.java
+++ str_n.java
@@ -3,7 +3,7 @@ public class Strn{
 
      public static void main(String []args){
         Scanner s=new Scanner(System.in);
-        String str=s.nextLine();
+        String str=s.next();
         int n=s.nextInt();
         for(int i=0; i<n;i++)
         {
