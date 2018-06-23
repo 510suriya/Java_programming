0a6c0e5e6199e43188a3713134fd29ea9407d6d6
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-23 21:18:00 +0530

    Create num_round.java

---
+++ num_round.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class num_round{
+
+     public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        float n=s.nextFloat();
+        int r;
+        r=(int)n;
+        System.out.println(r+1);
+      
+     } 
+     
+}
