f3495e4c98dedb9f4cc9e8b74901b98a158ef9d1
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-23 18:14:23 +0530

    Create near_powtwo.java

---
+++ near_powtwo.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class near_powtwo{
+
+     public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        int res;
+        int n=s.nextInt();
+        if(n%2==0)
+        {
+             res=2*n;
+             System.out.println(res);
+        }
+        else
+        System.out.println("not even");
+        
+      
+     } 
+     
+}
