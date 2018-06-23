e06f6a42144f5e3c46287a8d4acaa767fc63598e
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-23 11:49:36 +0530

    Update string_print.java

--- string_print.java
+++ string_print.java
@@ -4,6 +4,7 @@ public class print_str1{
      public static void main(String []args){
         Scanner s=new Scanner(System.in);
         String str=s.next();
+        str+=s.nextLine();
         for(int i=1;i<2;System.out.print(str)){
             i++;
         }
