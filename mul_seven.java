faf04de83c2a1c981fe1f686439ebd8e1f9da022
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-23 18:03:26 +0530

    Create mul_seven.java

---
+++ mul_seven.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class mul_seven{
+
+     public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        if(n%7==0)
+        {
+              System.out.println("yes");
+        }
+        else
+        System.out.println("no");
+     }  
+       
+     
+}
