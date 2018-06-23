cb05eeaeb49a20a5ffeb6f622742952e65280c2d
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-23 11:19:01 +0530

    Create num_small.java

---
+++ num_small.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class num_sort{
+
+     public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        int a[]=new int[10];
+         for(int i=0;i<10;i++){
+             a[i]=s.nextInt();
+            
+         }
+        Arrays.sort(a);
+        System.out.println(a[0]);
+        
+     }
+}
