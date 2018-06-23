e489d20d4c54de5c1fc790006b9b764d632a6836
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-23 11:39:59 +0530

    Create string_print.java

---
+++ string_print.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class print_str1{
+
+     public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        String str=s.next();
+        for(int i=1;i<2;System.out.print(str)){
+            i++;
+        }
+     }
+}
