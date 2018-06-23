f4f416a18ed2a37346033aae3b07fc961575305a
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-23 18:17:10 +0530

    Update near_powtwo.java

--- near_powtwo.java
+++ near_powtwo.java
@@ -5,15 +5,11 @@ public class near_powtwo{
         Scanner s=new Scanner(System.in);
         int res;
         int n=s.nextInt();
-        if(n%2==0)
+        if(n>0)
         {
              res=2*n;
              System.out.println(res);
         }
-        else
-        System.out.println("not even");
-        
-      
      } 
      
 }
