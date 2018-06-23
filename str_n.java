69ec13357a835a5276e492a7794bdc6790368e06
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-23 10:40:46 +0530

    Create str_n.java

---
+++ str_n.java
@@ -0,0 +1,15 @@
+import java.util.*;
+public class Strn{
+
+     public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        String str=s.nextLine();
+        int n=s.nextInt();
+        for(int i=0; i<n;i++)
+        {
+        char ch = str.charAt(i);
+        System.out.print(ch);
+        }
+        
+     }
+}
