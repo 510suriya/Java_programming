544584f2e78e4abd9c8f209d0093df4ecd75497c
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-23 18:36:40 +0530

    Create string_vow.java

---
+++ string_vow.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class string_vow{
+
+     public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        String s1;;
+        s1=s.next();
+        if(s1.contains("a")||s1.contains("e")||s1.contains("i")||s1.contains("o")||s1.contains("u"))
+        {
+              System.out.println("yes");
+        }
+        else
+        System.out.println("no");
+        
+      
+     } 
+     
+}
