f863c3d5cbedecd7fa417b204a9bee4e8b016d19
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-26 12:12:57 +0530

    Create isogram.java

---
+++ isogram.java
@@ -0,0 +1,23 @@
+import java.util.*;
+public class isogram{
+
+     public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        int i=0,j=0,c=0;
+        String str=s.next();
+        char[] ch=str.toCharArray();
+        for(i=0;i<str.length();i++){
+        for(j=i+1;j<str.length();j++){
+              if(ch[i]==ch[j])
+              c++;
+        }
+       }
+       if(c==0)
+       System.out.print("yes");
+       else
+       System.out.print("no");
+       
+      
+    
+     }    
+}
