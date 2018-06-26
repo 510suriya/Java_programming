66097ad5235e41c29e1a5da5a7064c71937b52fa
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-26 11:17:34 +0530

    Create str_oddEve.java

---
+++ str_oddEve.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class odd_evepos{
+
+     public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        int i=0;
+        String str=s.next();
+        char[] c=str.toCharArray();
+        for(i=0;i<str.length();i++)
+        if(i%2==0)
+        {
+        System.out.print(c[i]);
+       }
+       else{
+        System.out.print(c[i]+"\t"); 
+       }
+    
+     }    
+}
