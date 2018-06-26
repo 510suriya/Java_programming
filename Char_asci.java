36546933b1fd85052b60447c60f369f1fa72c882
510suriya <36623890+510suriya@users.noreply.github.com>
2018-06-26 10:41:51 +0530

    Create Char_asci.java

---
+++ Char_asci.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class asci{
+
+     public static void main(String []args){
+        Scanner s=new Scanner(System.in);
+        char c=s.next().charAt(0);
+        int asci = (int) c;
+        System.out.println(asci);
+   }
+}
