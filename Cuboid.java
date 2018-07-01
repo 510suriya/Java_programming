323b697d20669303b6e730df5c559092ad2976ec
510suriya <36623890+510suriya@users.noreply.github.com>
2018-07-01 17:46:07 +0530

    Add files via upload

---
+++ Cuboid.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Cuboid {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int x=s.nextInt();
+		int y=s.nextInt();
+		int z=s.nextInt();
+		int sur=2*x*y+1*y*z+2*y*z;
+		int vol=x*y*z;
+		System.out.println(sur);
+		System.out.println(vol);
+
+	}
+
+}
