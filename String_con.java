f402c22da5bacb79a5813de3c2640afb88aa3242
510suriya <36623890+510suriya@users.noreply.github.com>
2018-07-28 19:35:56 +0530

    Add files via upload

---
+++ String_con.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class String_con{
+
+
+public static void main(String []args){
+     
+Scanner s=new Scanner(System.in);
+       
+String s1=s.next();
+      
+String s2=s.next();
+     
+String s3=s1+s2;
+    
+ System.out.println(s3);
+
+ }
+
+}
