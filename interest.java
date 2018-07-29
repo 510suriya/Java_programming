d9cb6d50a7ae9c2cecae34ef76ee7538989012f4
510suriya <36623890+510suriya@users.noreply.github.com>
2018-07-29 20:15:15 +0530

    Update interest.java

--- interest.java
+++ interest.java
@@ -1,23 +1,12 @@
 import java.util.Scanner;
-
-import java.lang.*;
-
 public class Inter{
-
-     
 public static void main(String []args){
-         
 Scanner s=new Scanner(System.in);
-         
 int p=s.nextInt();
-         
 int n=s.nextInt();
-         
 int t=s.nextInt();
-         
 int i=(p*n*t)/100;
-         
-System.out.println(Math.floor(i));
+System.out.println(i);
         
      
 }
