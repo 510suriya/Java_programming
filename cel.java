e94613333d9ce1fcfc7926d3a2765be5010020c2
510suriya <36623890+510suriya@users.noreply.github.com>
2018-07-29 19:29:50 +0530

    Add files via upload

---
+++ cel.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+public class Cel_kel{
+
+    
+ public static void main(String []args){
+         
+Scanner s=new Scanner(System.in);
+        
+ int cel=s.nextInt();
+         
+double f=273.15;
+         
+double kel=cel+f;
+         
+System.out.println(kel);
+        
+     
+}
+}
