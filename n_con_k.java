75f6d42348e20b1ae4491225edc3622b78a7c73b
510suriya <36623890+510suriya@users.noreply.github.com>
2018-08-08 15:21:11 +0530

    Add files via upload

---
+++ n_con_k.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class n_con_k{
+
+     public static void main(String []args){
+         Scanner s=new Scanner(System.in);
+         String n=s.next();
+         String k=s.next();
+         System.out.println(n+k);
+     }
+}
